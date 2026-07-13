6e4eab3de20daa9e4f9026206fa46974df9fcc3e
Marcel Nague <marcelnague@hotmail.com>
2026-07-13 09:08:11 +0200

    fix(libs): exclude macOS metadata files and force-overwrite on extraction
    tar was failing on Windows when ._* resource fork files from macOS-built
    tarballs already existed or couldn't be created, causing yarn to roll back
    the entire package installation on a fresh yarn install.

--- libs.sh
+++ libs.sh
@@ -27,7 +27,7 @@ fi
 
 if [ "$DOWNLOAD" = true ]; then
     curl -L --silent "${URL}" -o "${DEST}"
-    tar -xvf "${DEST}"
+    tar --overwrite --exclude='._*' -xf "${DEST}"
     rm -f "${DEST}"
 
     echo "${VERSION}" > ${LOCK}
